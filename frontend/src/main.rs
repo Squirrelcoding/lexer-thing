89755b0d7e03147f7f136e378f29e873cae42680
Notsquirrelcoding <your_email@example.com>
2022-11-11 14:21:48 -0600

    defrg

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,16 +4,15 @@ use langlib::{interpreter::Interpreter, lexer::Lexer, parser::Parser};
 
 fn main() -> Result<(), langlib::interpreter::Err> {
     let s = "
-    if (2 == 5) {
-        print \"2 is equal to 5\";
-        print \"The univese makes no sense.\";
-    } else {
-        print \"2 is NOT equal to 5.\";
-        print \"As it should be.\";
-    }
+    let x = !(\"this is a string.\" == \"this is another string.\"); 
+    print (23-5)/ 2; 
+    let y =  (2 + 4) / 2; 
+    let z = !true; 
+    
+    print \"This is a very cool string.\"; let undefinedVar;
     ";
     let tokens = Lexer::new(s).tokenize().unwrap();
-    let ast = Parser::new(tokens).stmt()?;
+    let ast = Parser::new(tokens).get_statements();
 
     println!("{ast:?}");
 
